088935118f2205bc7281ced613e0170f73cdeb3d
Pranav Mahajan <parumahajan3000@gmail.com>
2025-11-28 16:33:11 +0530

    Basics commit

---
+++ BridgeLabz_Training/Basics.cs
@@ -0,0 +1,28 @@
+﻿// Using if-statements to check if the number is less than a specific number or not.
+
+using System;
+
+namespace BridgeLabz_Training
+{
+    public class Basics
+    {
+        public static void Main(string[] args)
+        {
+            int a = 10;
+            int b = 20;
+
+            if (a < 20)
+            {
+
+                // STRING CONCATENATION
+                Console.WriteLine("a: " + a);
+
+                // STRING INTERPOLATION
+                Console.WriteLine($"a: {a}");
+
+                // STRING FORMATTING (using placeholder {})
+                Console.WriteLine("a: {0}, b: {1}", a, b);
+            }
+        }
+    }
+}     
