d5b61e039966d583607228a337331149e647cac6
Pranav Mahajan <parumahajan3000@gmail.com>
2025-12-09 17:17:22 +0530

    updating today's work

---
+++ BridgeLabz_Training/NUnit/Basics.cs
@@ -0,0 +1,39 @@
+﻿/*
+---------------------------------------------------
+** NUnit **
+
+-> It is a unit testing framework through which we can write the code, with which can test our code.
+
+-> It is an open-source unit testing framework.
+----------------------------------------------------
+ATTRIBUTES:
+
+[Test] → marks a method as a test
+
+[TestFixture] → marks a class that contains tests
+
+[SetUp] → runs before each test (initialize objects)
+
+[TearDown] → runs after each test (cleanup)
+
+[TestCase(...)] → creates parameterized tests
+---------------------------------------------------
+ASSERTIONS (E-N-T)
+ 
+1) Assert.AreEqual(expected, actual)
+2) Assert.AreNotEqual(expected, actual)
+
+3) Assert.IsNull(obj)
+4) Assert.IsNotNull(obj)
+
+5) Assert.IsTrue(condition)
+6) Assert.IsFalse(condition)
+
+7) Assert.Throw <ExceptionType>( () => Code);
+---------------------------------------------------
+// => ->  (goes to/maps to) (Lambda operator)
+
+
+
+
+*/
